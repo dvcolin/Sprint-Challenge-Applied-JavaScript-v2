150a84a369a01f718f09b4b2e58a6756ca6b6c62
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-07-12 08:15:30 -0700

    Created header component. Appended header component to headerContainer component

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,7 +9,33 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(Header());
+
+function Header() {
+    //Elements
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const headerTitle = document.createElement('h1');
+    const temperature = document.createElement('span');
+
+    //Element structure
+    header.appendChild(date);
+    header.appendChild(headerTitle);
+    header.appendChild(temperature);
+
+    //Add classes
+    header.classList.add('header');
+    date.classList.add('date');
+    temperature.classList.add('temp');
+
+    //Element content
+    date.textContent = `MARCH 28, 2019`;
+    headerTitle.textContent = `Lambda Times`;
+    temperature.textContent = `98°`;
+
+    return header;
+}
 
 
 
